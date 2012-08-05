297955e6cce05ed4c62a89effa12e05719325558
sangwoohan <sangwoohan@ubuntu.ubuntu-domain>
2012-08-05 01:23:50 -0700

    moving moving

--- wanderer/coordinate.py
+++ wanderer/coordinate.py
@@ -1,8 +1,46 @@
 import random
+from wanderer import db
+from wanderer import History, Location
+from wanderer import gcd
 
 
-def move(lag,log):
-    return lag,log
+def move():
+    p_entry = History.query.descending('mongo_id').first()
+    n_entry = History()
+    n_entry = p_entry
+    #if the bot is not moving
+    if p_entry.status is "stop":
+    	print "Fitz Owen is not moving, and.."
+    	#if the bot has got a new suggestion, then move move move yo
+    	if p_entry.next_location.geocode is not p_entry.suggested_location.geocode:
+    		print "there is a suggested destination :" + p_entry.suggested_location.name
+    		n_entry.status = "move"
+    		n_entry.previous_location = p_entry.next_location
+    		n_entry.next_location = p_entry.suggested_location
+    		distance = gcd(n_entry.previous_location.geocode[0], n_entry.previous_location.geocode[1], n_entry.next_location.geocode[0], n_entry.next_location.geocode[1])
+    		n_entry.distance = distance
+    		n_entry.number_of_segments = int(distance / p_entry.speed)
+    		print "Now moving towards" + n_entry.next_location.name +"..."
+    #if the bot is approaching the destination..
+    elif P_entry.status is "move":
+    	if p_entry.segment is p_entry.number_of_segments:
+    		#then stop, and calibrate the location, reset
+    		n_entry.status = "stop"
+    		n_entry.current_location = p_entry.next_location
+    		n_entry.segment = 1
+    	#Okay, now let's move..
+    	else:
+    		n_entry.current_location.geocode[0] = ((p_entry.next_location.geocode[0] - p_entry.previous_location.geocode[0]) * p_entry.segment) / p_entry.number_of_segments
+    		n_entry.current_location.geocode[1] = ((p_entry.next_location.geocode[1] - p_entry.previous_location.geocode[1]) * p_entry.segment) / p_entry.number_of_segments
+    		n_entry.segment = p_entry.segment + 1
+    
+    n_entry.save()
+    			
+    
+    
+    
+    		
+
     
 
 
--- wanderer/models.py
+++ wanderer/models.py
@@ -3,13 +3,23 @@ from flaskext.mongoalchemy import MongoAlchemy
 from wanderer import db
 
 class History(db.Document):
-    previous_location = db.GeoField()
-    current_location = db.GeoField()
-    next_location = db.GeoField(allow_none=True)
+    previous_location = db.DocumentField(Location)
+    current_location = db.DocumentField(Location)
+    suggested_location = db.DocumentField(Location)
+    next_location = db.DocumentField(Location,allow_none = True)
     current_time = db.TimeField()
     speed = db.FloatField()
+    status = db.StringField()
+    distance = db.FloatField()
+    segment_index = db.IntField()
+    number_of_segments = db.IntField()
     def __str__(self):
         return '%s %s %s' % (self.destination, self.time, self.speed)
+
+
+class Location(db.Document):
+	name = db.StringField()
+	geocode = db.GeoField()
     
     
 
